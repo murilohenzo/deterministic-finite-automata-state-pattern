3d641c1cac63ca23b0681e956f50ad4a8965c2cd
murilo henzo <murilohezo@gmail.com>
2019-09-16 16:59:29 -0300

    versao final

--- DFA/dfa_exemplo.py
+++ DFA/dfa_exemplo.py
@@ -11,4 +11,4 @@ p1['Q1'][1] = 'Q0'
 p1['Q2'][0] = 'Q1'
 p1['Q2'][1] = 'Q2'
 
-print(AutomatoFinitoDeterministico.validarLinguagem(p1, 'Q0', {'Q0'}, '1011101'))
+print(AutomatoFinitoDeterministico.validarLinguagem(p1, 'Q0', {'Q0'}, [1,0,1,1,1,0,1]))
